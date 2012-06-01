1d16638bac1e733a36fe2afd056c21419122ae27
pahaz <pahaz.blinov@gmail.com>
2012-06-02 05:19:32 +0600

    Security fix and add Admin support
    Add Admin interface support.

--- filebrowser/decorators.py
+++ filebrowser/decorators.py
@@ -1,19 +1,18 @@
 # coding: utf-8
 
 # django imports
-from django.contrib.sessions.models import Session
-from django.shortcuts import get_object_or_404, render_to_response
+from django.contrib.auth import get_user
+from django.shortcuts import get_object_or_404
 from django.contrib.auth.models import User
-from django.template import RequestContext
 from django.conf import settings
-
+from django.utils.importlib import import_module
 
 def flash_login_required(function):
     """
     Decorator to recognize a user  by its session.
     Used for Flash-Uploading.
     """
-    
+
     def decorator(request, *args, **kwargs):
         try:
             engine = __import__(settings.SESSION_ENGINE, {}, {}, [''])
@@ -24,6 +23,19 @@ def flash_login_required(function):
         user_id = session_data['_auth_user_id']
         # will return 404 if the session ID does not resolve to a valid user
         request.user = get_object_or_404(User, pk=user_id)
+        return function(request, *args, **kwargs)
+    return decorator
+
+def flash_session_and_user_add(function):
+    """
+    Decorator to recognize a user and session by post session_key parameter.
+    """
+    
+    def decorator(request, *args, **kwargs):
+        engine = import_module(settings.SESSION_ENGINE)
+        session_key = request.POST.get('session_key')
+        request.session = engine.SessionStore(session_key) # request.session
+        request.user = get_user(request)
         return function(request, *args, **kwargs)
     return decorator
 
--- filebrowser/models.py
+++ filebrowser/models.py
@@ -1,1 +1,23 @@
-# This file is only necessary for the tests to work
+__author__ = 'pahaz'
+
+from django.db import models
+from django.core.exceptions import SuspiciousOperation
+from django.utils.translation import ugettext as _
+
+class FFile(models.Model):
+    """
+        Faking a model to use filebrowse in
+        admin site (without other hacks). I hack here
+        to avoid the user to hack forward. 
+
+        The name FFile avoid ugly-url (FakedFileModel)
+    """
+    class Meta:
+        verbose_name , verbose_name_plural = _("File") , _("Files")
+        managed = False
+
+    def save(self, *args, **kwargs):
+        raise SuspiciousOperation("filebrowser model is faked. Sorry to disappoint you...")
+
+    def delete(self, *args, **kwargs):
+        raise SuspiciousOperation("filebrowser model is faked. Sorry to disappoint you...")
